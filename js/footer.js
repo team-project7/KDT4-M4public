9564473f6f2eec7bd259e127e0a4caf06bbb27ca
sung34 <imspins1234@gmail.com>
2023-02-03 22:00:44 +0900

    feat:

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,5 @@
 const footerHTML = document.createElement('footer')
-footerHTML.innerHTML = 
-`
+footerHTML.innerHTML = /*html*/ `
 <footer class="footer mo" data-v-a1b21a74="" data-v-850ee4a8="">
       <div class="inner" data-v-a1b21a74="">
         <div class="service_area" data-v-a1b21a74="">
@@ -147,53 +146,39 @@ footerHTML.innerHTML =
           </ul>
           <div class="footer_sns" data-v-a1b21a74="">
             <div class="sns_box" data-v-a1b21a74="">
-              <a href="#" aria-label="인스타그램" class="sns" data-v-a1b21a74=""
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="ico-instagram icon sprite-icons"
-                  data-v-a1b21a74=""
-                >
-                  <use
-                    href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-ico-instagram"
-                    xlink:href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-ico-instagram"
-                    data-v-a1b21a74=""
-                  ></use></svg></a
-              ><a href="#" aria-label="페이스북" class="sns" data-v-a1b21a74=""
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="ico-facebook icon sprite-icons"
-                  data-v-a1b21a74=""
-                >
-                  <use
-                    href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-ico-facebook"
-                    xlink:href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-ico-facebook"
-                    data-v-a1b21a74=""
-                  ></use></svg></a
-              ><a href="#" aria-label="카카오톡" class="sns" data-v-a1b21a74=""
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="ico-talk icon sprite-icons"
-                  data-v-a1b21a74=""
-                >
-                  <use
-                    href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-ico-talk"
-                    xlink:href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-ico-talk"
-                    data-v-a1b21a74=""
-                  ></use></svg
-              ></a>
+              <a href="#" aria-label="인스타그램" class="sns" data-v-a1b21a74="">
+                <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 11C3 7.22876 3 5.34315 4.17157 4.17157C5.34315 3 7.22876 3 11 3H13C16.7712 3 18.6569 3 19.8284 4.17157C21 5.34315 21 7.22876 21 11V13C21 16.7712 21 18.6569 19.8284 19.8284C18.6569 21 16.7712 21 13 21H11C7.22876 21 5.34315 21 4.17157 19.8284C3 18.6569 3 16.7712 3 13V11Z" stroke="#222222"/>
+                <circle cx="16.5" cy="7.5" r="1.5" fill="#222222"/>
+                <circle cx="12" cy="12" r="3.5" stroke="#222222"/>
+                </svg>
+              </a>
+              <a href="#" aria-label="페이스북" class="sns" data-v-a1b21a74="">
+                  <svg fill="#000000" height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512" xml:space="preserve">
+                  <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M169.5,357.6l-2.9,38.3h-39.3
+                    v133H77.7v-133H51.2v-38.3h26.5v-25.7c0-11.3,0.3-28.8,8.5-39.7c8.7-11.5,20.6-19.3,41.1-19.3c33.4,0,47.4,4.8,47.4,4.8l-6.6,39.2
+                    c0,0-11-3.2-21.3-3.2c-10.3,0-19.5,3.7-19.5,14v29.9H169.5z"/>
+                  </svg>
+                </a>
+                <a href="#" aria-label="카카오톡" class="sns" data-v-a1b21a74="">
+                  <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <g>
+                          <path fill="none" d="M0 0h24v24H0z"/>
+                          <path d="M12 3c5.799 0 10.5 3.664 10.5 8.185 0 4.52-4.701 8.184-10.5 8.184a13.5 13.5 0 0 1-1.727-.11l-4.408 2.883c-.501.265-.678.236-.472-.413l.892-3.678c-2.88-1.46-4.785-3.99-4.785-6.866C1.5 6.665 6.201 3 12 3zm5.907 8.06l1.47-1.424a.472.472 0 0 0-.656-.678l-1.928 1.866V9.282a.472.472 0 0 0-.944 0v2.557a.471.471 0 0 0 0 .222V13.5a.472.472 0 0 0 .944 0v-1.363l.427-.413 1.428 2.033a.472.472 0 1 0 .773-.543l-1.514-2.155zm-2.958 1.924h-1.46V9.297a.472.472 0 0 0-.943 0v4.159c0 .26.21.472.471.472h1.932a.472.472 0 1 0 0-.944zm-5.857-1.092l.696-1.707.638 1.707H9.092zm2.523.488l.002-.016a.469.469 0 0 0-.127-.32l-1.046-2.8a.69.69 0 0 0-.627-.474.696.696 0 0 0-.653.447l-1.661 4.075a.472.472 0 0 0 .874.357l.33-.813h2.07l.299.8a.472.472 0 1 0 .884-.33l-.345-.926zM8.293 9.302a.472.472 0 0 0-.471-.472H4.577a.472.472 0 1 0 0 .944h1.16v3.736a.472.472 0 0 0 .944 0V9.774h1.14c.261 0 .472-.212.472-.472z"/>
+                      </g>
+                  </svg>
+                </a>
             </div>
             <button class="btn_business" data-v-a1b21a74="">
               사업자정보
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="arr-down icon sprite-icons"
-                data-v-a1b21a74=""
-              >
-                <use
-                  href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-arr-down"
-                  xlink:href="/_nuxt/3182c3b1ca2f77da7bc3e1acf109306c.svg#i-arr-down"
-                  data-v-a1b21a74=""
-                ></use>
+              <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                width="12px" height="12px" viewBox="0 0 451.847 451.847"
+                xml:space="preserve">
+              <g>
+                <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751
+                  c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0
+                  c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
+              </g>
               </svg>
             </button>
           </div>
@@ -201,17 +186,29 @@ footerHTML.innerHTML =
             <div class="info_list" data-v-a1b21a74="">
               <dl class="info_item" data-v-a1b21a74="">
                 <dt class="business_title" data-v-a1b21a74="">
-                  크림 주식회사 · 대표 김창욱<br />사업자등록번호 : 570-88-01618
-                  <a
-                    href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=5708801618"
-                    >사업자정보확인</a
-                  ><br />통신판매업 : 제 2021-성남분당C-0093호<br />사업장소재지
-                  : 경기도 성남시 분당구 분당내곡로 131 판교테크원 타워1, 8층<br />호스팅
-                  서비스 : 네이버 클라우드 ㈜
+                  크림 주식회사 · 대표 김창욱 
+                  <span class="blank"></span> 
+                  사업자등록번호 : 570-88-01618
+                  <a href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=5708801618">
+                    사업자정보확인
+                  </a>
+                  <span class="blank"></span>
+                  통신판매업 : 제 2021-성남분당C-0093호
+                  <span class="blank"></span>
+                  사업장소재지 : 경기도 성남시 분당구 분당내곡로 131 판교테크원 타워1, 8층
+                  <span class="blank"></span>
+                  호스팅 서비스 : 네이버 클라우드 ㈜
                 </dt>
               </dl>
             </div>
           </div>
+        </div>
+        <div class="notice_guarantee" data-v-a1b21a74="">
+          <p class="title" data-v-a1b21a74="">신한은행 채무지급보증 안내</p>
+          <p class="description" data-v-a1b21a74=""> 
+            당사는 고객님의 현금 결제 금액에 대해 신한은행과 채무지급보증 계약을 체결하여 안전거래를 보장하고 있습니다.
+            <a data-v-a1b21a74="" href="#" class="link_guarantee">서비스가입 사실 확인</a>
+          </p>
         </div>
         <div class="notice_area" data-v-a1b21a74="">
           <p class="notice" data-v-a1b21a74="">
@@ -224,7 +221,6 @@ footerHTML.innerHTML =
           <p class="copyright" data-v-a1b21a74="">© KREAM Corp.</p>
         </div>
       </div>
-      <!----><!----><!----><!----><!---->
     </footer>
 `
 
