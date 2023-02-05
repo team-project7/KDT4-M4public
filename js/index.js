397912be4f8a2cc69d40fcc789d322f1a9c60c85
jyc-coder <jyc4648@gmail.com>
2023-02-05 23:58:49 +0900

    feat: Add Input Validation
    사용자가 id와 pw를 입력할때 유효성검사 기능을 추가함.
    
    - navigo를 사용해서 login 과 join 링크에 해당하는 엘리먼트를 렌더링할수 있는 메서드 추가(appendLogin(), appendJoin())
    
    - appendLogin메서드는 로그인 엘리먼트 렌더링, appendJoin메서드는 회원가입 엘리먼트 렌더링 메서드이며 로그인, 회원가입 기능은 추후에 추가될 예정
    
    - 로그인 페이지의 input 유효성 검사 기능은 구현된 상태, 이메일, 비밀번호를 조건에 맞게 입력하지 않으면 로그인 버튼이 활성화되지 않게 구현함.
    
    - 결과 확인을 위해 이동이 편이하도록 메인화면 버튼을 만들었지만 최종 결과 제작후 제거할 예정

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-import appendFooter from "./footer";
+import appendFooter from './footer'
+import Navigo from 'navigo'
+import appendLogin from './login'
+import appendJoin from './join'
+const router = new Navigo('/')
 
-appendFooter();
+appendFooter()
+
+router
+  .on('/login', function () {
+    document.body.innerHTML = ''
+    appendLogin()
+    appendFooter()
+  })
+  .on('/join', function () {
+    document.body.innerHTML = ''
+    appendJoin()
+    appendFooter()
+  })
+  .resolve()
