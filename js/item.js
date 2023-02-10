e806c83dadf2bde90e7001a47dee02730c9426f9
jyc-coder <jyc4648@gmail.com>
2023-02-10 15:43:52 +0900

    feat: Edit items.js
    남성 태그관련 제품목록만 렌더링 할수 있도록 
    searchByTag 메소드를 사용해서 나온 배열 데이터 maleData 만큼 렌더링되게 수정함
    (추후 더보기 기능을 통해 전체를 열람할수 있도록 수정할 예정, 일단 렌더링이 제대로 나타나는지를 확인하기 위한 작업)

--- js/item.js
+++ js/item.js
@@ -3,24 +3,25 @@ import rolex from '../image/brandFocus_rolex.jpg'
 import { searchAll, searchByTag } from './request'
 /** 제품 아이템을 렌더링 하는 메소드 */
 export async function appendItem() {
-  // 데이터 확인
-  // const productData = await searchAll()
-  // console.log(productData)
-  // const maleData = await searchByTag(' 남성')
-  // console.log(maleData)
+  const maleData = await searchByTag(' 남성')
+  console.log(maleData)
 
   const productListEl = document.querySelector('.product__list')
+  // 제품 아이템이 들어있는 리스트 엘리먼트
   const productFirstListEl = document.createElement('div')
   productFirstListEl.className = 'product__list__first'
-
-  productFirstListEl.innerHTML = /*html */ `
-           <div class="product__item">
+  productListEl.append(productFirstListEl)
+  // 제품 아이템 엘리먼트
+  maleData.map((e) => {
+    const productItemEl = document.createElement('div')
+    productItemEl.classList = 'product__item'
+    productItemEl.innerHTML = /*html */ `
             <a class="product__item__inner" href="#">
               <div class="thumb_box">
                 <div class="item">
                   <picture class="item__img">
                     <img
-                      src=${rolex}
+                      src=${e.thumbnail}
                       alt="제품이미지"
                     />
                   </picture>
@@ -31,19 +32,19 @@ export async function appendItem() {
               </div>
               <div class="info_box">
                 <div class="brand">
-                  <p class="brand__text">nike</p>
+                  <p class="brand__text">${e.title}</p>
                 </div>
-                <p class="name">nike x tom skdjflskdjflksjd</p>
+                <p class="name">${e.description}</p>
                 <div class="price">
                   <div class="amount">
-                    <em class="amount_num">137,000</em>
+                    <em class="amount_num">${e.price}</em>
                   </div>
                 </div>
               </div>
             </a>
-          </div>
   `
-  productListEl.append(productFirstListEl)
+    productFirstListEl.append(productItemEl)
+  })
 
   // 여기는 한번만 렌더링
   const moreListEl = document.createElement('div')
