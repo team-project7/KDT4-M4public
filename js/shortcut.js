ab4c257857d57fba226dfe93af072d3a4aa6796f
SedanLee <water8048@naver.com>
2023-02-10 17:19:20 +0900

    feat: shortcut 추가

---
+++ js/shortcut.js
@@ -0,0 +1,24 @@
+import { shortcutItems } from './shortcutItems'
+
+CreateShortcut()
+
+function CreateShortcut() {
+  const shortcutEl = document.createElement('div')
+  shortcutEl.classList.add('shortcut_items_wrap')
+
+  shortcutItems.map((item) => {
+    const shortcutItem = document.createElement('div')
+    shortcutItem.classList.add('shortcut_item')
+    shortcutItem.innerHTML = `
+      <a href="${item.href}">
+        <div class="shortcut_item_img_wrap">
+          <img src="${item.img}" class="shortcut_item_img_bg" alt="${item.itemTitle}">
+        </div>
+        <h4>${item.title}</h4>
+      </a>
+  `
+
+    shortcutEl.append(shortcutItem)
+  })
+  document.body.append(shortcutEl)
+}
