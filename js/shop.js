6f73c64fed93ccf634dcead1438e7a3172f09c02
jyc-coder <jyc4648@gmail.com>
2023-02-15 21:12:40 +0900

    feat: Added appendSmallProducts, appendSmallItems
    상점페이지의 제품목록의 크기를 메인 페이지보다 살짝 줄이기위해서, 더 작은 이미지 스타일 속성으로 렌더링 하는 appendSmallProducts, appendSmallItems 메소드 구현
    - 기존에 존재했던 메소드와의 차이점은 크기를 줄이고자하는 엘리먼트에 --sm을 붙여서 scss파일에 해당 속성을 추가해줌으로써 크기를 줄이는 원리로 구현함

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,38 @@
-import { appendProducts } from './products'
+import { appendSmallProducts } from './products'
+import plus from '/image/plus.png'
+export default function appendShopContent() {
+  const catList = ['카테고리', '브랜드', '성별', '가격']
+  const contentEl = document.createElement('div')
+  contentEl.className = 'content'
 
+  contentEl.innerHTML = /*html */ `
+    <div class="search_filter">
+        <div class="filter_status">
+          <div class="status_box">
+            <span class="status_text"> 필터 </span>
+          </div>
+        </div>
+     `
 
-export default function shop() {
+  document.body.append(contentEl)
+  const searchFilterEl = document.querySelector('.search_filter')
+  const filterListEl = document.createElement('div')
+  filterListEl.className = 'filter_list'
 
-    appendProducts()
+  catList.forEach((e) => {
+    const filterListEl = document.createElement('div')
+    filterListEl.className = 'filter_list'
+    filterListEl.innerHTML = /*html */ `
+     <div class="filter_title">
+            <div class="title_box">
+              <span class="main_title">${e}</span>
+              <div class="placeholder">모든 ${e}</div>
+            </div>
+            <div class="icon_box"><img class="more_icon" src=${plus}  alt="확장" /></div>
+          </div>
+      `
+
+    searchFilterEl.append(filterListEl)
+  })
+  appendSmallProducts(' 남성', 4, 12, contentEl)
 }
-
