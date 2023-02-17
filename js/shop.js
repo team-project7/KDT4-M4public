9e529fbf5e54b72a2a05efef382f953ce4e94a6d
jyc-coder <jyc4648@gmail.com>
2023-02-17 13:56:04 +0900

    feat: Added Filter Layout
    - 검색 레이아웃 필터 구현 완료 ( 세부 서브메뉴는 아직 구현하지 않음)
    - 추후 서브메뉴 추가 예정

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,7 @@
 import { appendSmallProducts } from './products'
 import plus from '/image/plus.png'
+import unchecked from '/image/unchecked.png'
+import checked from '/image/checked.png'
 export default function appendShopContent() {
   const catList = ['카테고리', '브랜드', '성별', '가격']
   const contentEl = document.createElement('div')
@@ -12,27 +14,39 @@ export default function appendShopContent() {
             <span class="status_text"> 필터 </span>
           </div>
         </div>
+    </div>
      `
 
   document.body.append(contentEl)
-  const searchFilterEl = document.querySelector('.search_filter')
+  appendFilterList('카테고리')
+  appendFilterList('브랜드')
+  appendFilterList('성별')
+  appendFilterList('가격')
+
+  appendSmallProducts(' 남성', 4, 12)
+}
+
+export function appendFilterList(category) {
   const filterListEl = document.createElement('div')
   filterListEl.className = 'filter_list'
-
-  catList.forEach((e) => {
-    const filterListEl = document.createElement('div')
-    filterListEl.className = 'filter_list'
-    filterListEl.innerHTML = /*html */ `
-     <div class="filter_title">
-            <div class="title_box">
-              <span class="main_title">${e}</span>
-              <div class="placeholder">모든 ${e}</div>
+  filterListEl.innerHTML = /*html */ `
+    <div class="filter_title">
+       <div class="title_box">
+              <span class="main_title">${category}</span>
+              <div class="placeholder">모든 ${category}</div>
             </div>
-            <div class="icon_box"><img class="more_icon" src=${plus}  alt="확장" /></div>
-          </div>
-      `
+            <div class="icon_box"><img src=${plus} alt="확장" /></div>
+    </div>
+  `
+  document.querySelector('.search_filter').append(filterListEl)
 
-    searchFilterEl.append(filterListEl)
+  const filterlistEls = document.querySelectorAll('.filter_list')
+
+  filterlistEls.forEach((e) => {
+    const filterMenuEl = e.querySelector('.filter_menu')
+    e.addEventListener('click', () => {
+      filterMenuEl.style.display =
+        filterMenuEl.style.display === 'block' ? 'none' : 'block'
+    })
   })
-  appendSmallProducts(' 남성', 4, 12, contentEl)
 }
