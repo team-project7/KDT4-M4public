73f9227fd9482c348a56a3ba28716308ef2cd555
sung34 <imspins1234@gmail.com>
2023-02-22 15:54:39 +0900

    init: payment page base

---
+++ js/payment/paymentRender.js
@@ -0,0 +1,39 @@
+export function renderPaymentContainer() {
+
+}
+
+function renderProductInfoArea() {
+
+}
+
+function renderDelieveryArea() {
+
+}
+
+function renderAddDeliveryInfoModal() {
+
+}
+
+function renderDelieveryProfilesModal() {
+
+}
+
+function renderPointArea() {
+
+}
+
+function renderOrderTotalArea() {
+
+}
+
+function renderPaymentMethodArea() {
+
+}
+
+function renderConfirmArea() {
+
+}
+
+function renderFinalConfirmModal() {
+  
+}
