a5856df6afe7f822d118411d7586ce2284be2160
wooseongjeon <wooseongjeon@gmail.com>
2023-02-22 16:22:13 +0900

    feat: modified detailedItem.js

--- js/detailedItem.js
+++ js/detailedItem.js
@@ -2,5 +2,7 @@
 
 function appendDetailedItem () {
   // 상세 품목 페이지 렌더링
-  
+  const detailedItem = document.createElement('div')
+  detailedItem.className = 'detailedItem'
+  detailedItem.innerHTML = /*HTML*/''
 }
