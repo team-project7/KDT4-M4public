207fabe40a731444fc92ad300f42e6a9cec40fd5
jyc-coder <jyc4648@gmail.com>
2023-02-22 16:21:09 +0900

    feat: Impliment Price Search
    - 가격 필터 리스트중 하나를 선택하면 해당 가격대를 만족하는 제품을 렌더링하는 기능 구현
    - appendMenu 메서드를 통해서 추가된 tag-name 데이터를 사용해서 `appendSmallProducts(tagname, 12, contentEl, 0)`메소드를 호출한다.

--- js/item.js
+++ js/item.js
@@ -1,6 +1,6 @@
 import wishOff from '../image/wish-off.png'
 import wishOn from '../image/wish-on.png'
-import { searchByTag } from './request'
+import { searchAll, searchByTag } from './request'
 
 /** 제품 아이템을 렌더링 하는 메소드 */
 export async function appendItem(tag, dpnum, num, container, listIndex) {
@@ -94,7 +94,26 @@ export async function appendItem(tag, dpnum, num, container, listIndex) {
 
 export async function appendSmallItem(tag, dpnum, listIndex) {
   const chunk = []
-  const resultData = await searchByTag(tag)
+  let resultData = []
+  // 가격별로 만족하는데이터 정렬
+  const allData = await searchAll()
+  switch (tag) {
+    case '10만원 이하':
+      resultData = allData.filter((e) => e.price <= 100000)
+      break
+    case '10만원-30만원 이하':
+      resultData = allData.filter((e) => e.price > 100000 && e.price <= 300000)
+      break
+    case '30만원-50만원 이하':
+      resultData = allData.filter((e) => e.price > 300000 && e.price <= 500000)
+      break
+    case '50만원 이상':
+      resultData = allData.filter((e) => e.price > 500000)
+      break
+    default:
+      resultData = await searchByTag(tag)
+      break
+  }
   chunk.push(resultData.slice(0, dpnum))
   for (let i = dpnum; i < resultData.length; i += 8) {
     chunk.push(resultData.slice(i, i + 8))
