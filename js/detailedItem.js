c7d4c38e1c075769078ad00429f755fbdc646f92
wooseongjeon <woos_94@naver.com>
2023-03-03 22:57:45 +0900

    feat: modified detailed.js

--- js/detailedItem.js
+++ js/detailedItem.js
@@ -373,7 +373,8 @@ function appendDetailedItem() {
       let json = await res.json()
       
       if(json.email) {
-        location.replace(`/products/payment` + `?productId=${id}`)
+        alert('현재 제품은 원 사이즈입니다.')
+        //location.replace(`/products/payment` + `?productId=${id}`)
       }else {
         alert('로그인 후에 다시 시도해주세요!')
         location.replace('/login')
