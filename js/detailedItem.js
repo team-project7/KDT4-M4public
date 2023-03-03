d9f9ecc439ccd2097d934d8172ae259c7274ab0b
wooseongjeon <woos_94@naver.com>
2023-03-03 23:30:13 +0900

    feat: modified detailedItem.js

--- js/detailedItem.js
+++ js/detailedItem.js
@@ -94,7 +94,7 @@ function appendDetailedItem() {
             <div class="item_size">
               <span class="size_txt">사이즈</span>
               <a href="javascript:void(0)" class="size_btn">
-                <span class="btn_txt">모든 사이즈(예시)</span>
+                <span class="btn_txt">모든 사이즈</span>
                 <span class="material-symbols-outlined">
                   arrow_drop_down
                 </span>
@@ -316,6 +316,17 @@ function appendDetailedItem() {
             </div>
           </div>
         </div>
+        <div class="size_popUp_background hide">
+          <div class="size_popUp">
+            <h3 class="popUp_title">사이즈</h3>
+            <span class="material-symbols-outlined popUp_close">
+              close
+            </span>
+            <div class="popUp_content">
+              구현중...
+            </div>
+          </div>
+        </div>
       `
 
     // 페이지 랜더링
@@ -356,6 +367,8 @@ function appendDetailedItem() {
 
     // 사이즈 체크
     const sizeCheckSlot = document.querySelector('a.size_btn')
+    const sizePopUp = document.querySelector('.size_popUp_background')
+    
 
     sizeCheckSlot.addEventListener('click', async () => {
       const res = await fetch(
@@ -373,14 +386,21 @@ function appendDetailedItem() {
       let json = await res.json()
       
       if(json.email) {
-        alert('현재 제품은 원 사이즈입니다.')
+        // alert('현재 제품은 원 사이즈입니다.')
         //location.replace(`/products/payment` + `?productId=${id}`)
+        sizePopUp.classList.remove('hide')
       }else {
         alert('로그인 후에 다시 시도해주세요!')
         location.replace('/login')
       }
       
       return json
+    })
+
+    const sizePopUpClose = document.querySelector('.size_popUp span.popUp_close')
+
+    sizePopUpClose.addEventListener('click', () => {
+      sizePopUp.classList.add('hide')
     })
 
     // 관심상품 
