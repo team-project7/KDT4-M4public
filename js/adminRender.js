20c747cce6dcba50865b57da0fec2a89634423e5
sung34 <imspins1234@gmail.com>
2023-03-07 23:48:42 +0900

    feat: Added searched item onclick edit/remove feature Added add item button on search area Added Home button on top left Removed aside bar due to no use

--- js/adminRender.js
+++ js/adminRender.js
@@ -1,5 +1,5 @@
 import  noProfile  from "../image/no-profile.png"
-
+import logo from "../image/kream-logo.png"
 /**
  * appends 
  * - modal for an item addition
@@ -10,6 +10,13 @@ import  noProfile  from "../image/no-profile.png"
  * @void
  */
 export function appendAdminPage() {
+  const homeBtn = document.createElement('button')
+  homeBtn.classList.add('admin-home-btn')
+  const img = new Image()
+  img.src = logo
+  homeBtn.append(img)
+  homeBtn.onclick = () => location.replace('/')
+
   const adminModalWrapper = document.createElement('div')
   adminModalWrapper.classList.add('admin-add-modal-wrapper')
   adminModalWrapper.setAttribute('id', 'admin-modal-add')
@@ -23,9 +30,9 @@ export function appendAdminPage() {
   const adminPageContainer = document.createElement('div')
   adminPageContainer.classList.add('admin-page')
   adminPageContainer.append(
+    homeBtn,
     adminModalWrapper,
     adminEditModalWrapper,
-    renderNavBar(),
     renderSearchArea(),
     renderSearchContainer()
   )
@@ -44,17 +51,17 @@ function renderAdminAddModal() {
   /*html*/`
     <section class="admin-modal-info">
       <div class="admin-modal-info__props">
-        <label for="admin-add-title-input">Title</label>
+        <label for="admin-add-title-input">・Title</label>
         <input id="admin-add-title-input" type="text" for="price" placeholder="Title"/>
-        <label for="admin-add-description-input">Desc</label>
+        <label for="admin-add-description-input">・Desc</label>
         <input id="admin-add-description-input" type="text" for="desc" placeholder="Item Description"/>
-        <label for="admin-add-price-input">Price</label>
+        <label for="admin-add-price-input">・Price</label>
         <input id="admin-add-price-input" type="text" for="price" placeholder="125200"/>
-        <label for="admin-add-tags-input">Tags</label>
+        <label for="admin-add-tags-input">・Tags</label>
         <input id="admin-add-tags-input" type="text" for="tags" placeholder="Tag1, Tag2, ..." />
       </div>
       <div class="admin-modal-info__image">
-        <label for="image">Image</label>
+        <label for="image">・Image</label>
         <input id="admin-add-file-input" type="file" for="image" />
         <canvas id="admin-add-canvas"></canvas>
       </div>
@@ -81,24 +88,26 @@ function renderAdminEditModal() {
   /*html*/`
     <section class="admin-modal-info">
       <div class="admin-modal-info__props">
+        <span>・ID</span>
         <span id="admin-edit-id"></span>
-        <label for="admin-edit-title-input">Title</label>
+        <label for="admin-edit-title-input">・Title</label>
         <input id="admin-edit-title-input" type="text" for="price" placeholder="Title"/>
-        <label for="admin-edit-description-input">Desc</label>
+        <label for="admin-edit-description-input">・Desc</label>
         <input id="admin-edit-description-input" type="text" for="desc" placeholder="Item Description"/>
-        <label for="admin-edit-price-input">Price</label>
+        <label for="admin-edit-price-input">・Price</label>
         <input id="admin-edit-price-input" type="text" for="price" placeholder="125200"/>
-        <label for="admin-edit-tags-input">Tags</label>
+        <label for="admin-edit-tags-input">・Tags</label>
         <input id="admin-edit-tags-input" type="text" for="tags" placeholder="Tag1, Tag2, ..." />
       </div>
       <div class="admin-modal-info__image">
-        <label for="image">Image</label>
+        <label for="image">・Image</label>
         <input id="admin-edit-file-input" type="file" for="image" />
         <canvas id="admin-edit-canvas"></canvas>
       </div>
     </section>
     <div class="admin-modal-btns">
       <div class="flex-space"></div>
+      <button id="admin-page-btns__remove-btn">REMOVE</button>
       <button id="admin-edit-modal-btns__edit-done-btn">EDIT</button>
       <button id="admin-edit-modal-btns__close-btns">X</button>
     </div>
@@ -113,7 +122,7 @@ function renderAdminEditModal() {
  * @returns Array.length ? <span>no result</span> : <ul class="admin-page-search"></ul>
  */
 export function renderSearchResult(resultArray) {
-  if(resultArray.length === 0) return document.createElement('span').innerHTML = "no result"
+  if(resultArray.length === 0) return document.createElement('span').innerHTML = "일치하는 항목이 없습니다."
   const searchResultList = document.createElement('ul')
   searchResultList.classList.add('admin-page-search')
   searchResultList.innerHTML = 
@@ -153,7 +162,7 @@ function renderItem(item, index) {
     <span class="admin-item-id">${item.id}</span>
     <span>${item.title}</span>
     <span>${item.description}</span>
-    <span>${item.price}</span>
+    <span>${item.price.toLocaleString()}원</span>
     <span>${item.tags}</span>
   `
   return searchItem
@@ -209,6 +218,7 @@ function renderSearchArea() {
       <button id="admin-search__search-all-users">모든 사용자 보기</button>
     </div>
     <span id="admin-search-count"></span>
+    <button id="admin-page-btns__add-btn">상품 추가</button>
   `
   return searchEl
 }
@@ -250,7 +260,7 @@ function renderSearchUser(user, index) {
   indexEl.textContent = `${index}`
   
   const imgWrapper = document.createElement('div')
-  imgWrapper.classList.add('admin-page-search__item-img')
+  imgWrapper.classList.add('admin-page-search__item-profile')
   
   const profileImg = new Image()
   profileImg.alt = "User Image"
